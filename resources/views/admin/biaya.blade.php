764c99b92eeb57915fbb5d4469f2368a9f32a729
whitewolf <wolfw1926@gmail.com>
2022-06-13 01:30:34 +0800

    membuat da menambah login page,

--- resources/views/admin/biaya.blade.php
+++ resources/views/admin/biaya.blade.php
@@ -1,4 +1,44 @@
 @extends('component.template')
 @section('konten')
 <h1>Halaman <strong style="color: brown;">Biaya</strong></h1>
+<div class="col-12">
+    <div class="card p-3">
+        <div class="row">
+            <div class="container-fluid">
+                <div class="card-haeder text-center border-bottom border-dark border-bottom-3">
+                    <h2 style="font-family:Arial, Helvetica, sans-serif  ;"> Biaya Kerja</h2>
+                </div>
+            </div>
+
+        </div>
+        <div class="row">
+
+            <table class="table table-borderless">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Rincian Pekerjaan</th>
+                        <th> Volume/satuan</th>
+                        <th>Harga Satuan</th>
+                        <th>Total</th>
+                        <th>Jumlah</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th>1.</th>
+                        <td>Pembangunan</td>
+                        <td>M2</td>
+                        <td>10.000.000</td>
+                        <td>5</td>
+                        <th>50.000.000</th>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+
+</div>
+<div class="row"></div>
 @endsection
---
+++ resources/views/login.blade.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{ $title }}</title>
+
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="{{ asset('plugins/fontawesome-free/css/all.min.css') }}">
+    <!-- icheck bootstrap -->
+    <link rel="stylesheet" href="{{ asset('plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{ asset('dist/css/adminlte.min.css') }}">
+    <style>
+        @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+            }
+        }
+    </style>
+</head>
+
+<body class="hold-transition login-page" style="background: url('/img/alat.jpg');background-size: cover; background-attachment: fixed; background-position: 0 -20rem;">
+    <div class="container p-5">
+        <div class="text-center m-3">
+        </div>
+        <div class="login-box mx-auto d-block bg-blue">
+            <div class="login-logo  text-light">
+                <a href="{{ asset('assets/index2.html') }}"><b>Login</b></a>
+            </div>
+            <!-- /.login-logo -->
+            <div class="card ">
+                <div class="card-body login-card-body">
+                    <p class="login-box-msg">Sign in to start your session</p>
+
+                    <form action="" method="post">
+                        <div class="input-group mb-3">
+                            <input type="text" class="form-control" placeholder="Username" name="username" required autocomplete="off">
+                            <div class="input-group-append">
+                                <div class="input-group-text">
+                                    <span class="fas fa-envelope"></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="input-group mb-3">
+                            <input type="password" class="form-control" placeholder="Password" name="password" required>
+                            <div class="input-group-append">
+                                <div class="input-group-text">
+                                    <span class="fas fa-lock"></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-8">
+                                <div class="icheck-primary">
+                                    <input type="checkbox" id="remember">
+                                    <label for="remember">
+                                        Remember Me
+                                    </label>
+                                </div>
+                            </div>
+                            <!-- /.col -->
+                            <div class="col-4">
+                                <button type="submit" class="btn btn-primary btn-block">Masuk</button>
+                            </div>
+                            <!-- /.col -->
+                        </div>
+                    </form>
+
+                    <!-- <div class="social-auth-links text-center mb-3">
+                    <p>- OR -</p>
+                    <a href="#" class="btn btn-block btn-primary">
+                        <i class="fab fa-facebook mr-2"></i> Sign in using Facebook
+                    </a>
+                    <a href="#" class="btn btn-block btn-danger">
+                        <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
+                    </a>
+                </div> -->
+                    <!-- /.social-auth-links -->
+
+                    <p class="mb-1">
+                        <a href="forgot-password.html">I forgot my password</a>
+                    </p>
+                    <p class="mb-0">
+                        <a href="register.html" class="text-center">Register a new membership</a>
+                    </p>
+                </div>
+                <!-- /.login-card-body -->
+            </div>
+        </div>
+    </div>
+    <!-- /.login-box -->
+
+    <!-- jQuery -->
+    <script src="{{ asset('plugins/jquery/jquery.min.js') }}"></script>
+    <!-- Bootstrap 4 -->
+    <script src="{{ asset('plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <!-- AdminLTE App -->
+    <script src="{{ asset('dist/js/adminlte.min.js') }}"></script>
+</body>
+
+</html>
