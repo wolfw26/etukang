b86191ab3d316e65ba26b7d847f3811501a75725
whitewolf <wolfw1926@gmail.com>
2022-06-20 18:16:17 +0800

    create material table

---
+++ database/migrations/2022_06_20_101317_create_material_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('material', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_material');
+            $table->string('satuan');
+            $table->int('harga_satuan');
+            $table->int('jumlah');
+            $table->int('jumlah_harga');
+            $table->foreignId('proyek_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('material');
+    }
+};
