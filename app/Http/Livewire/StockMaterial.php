1bee7e1512a579782b513892cc7c05a5c5a2acf6
whitewolf <wolfw1926@gmail.com>
2022-07-13 20:14:12 +0800

    Make livewire stockMaterial

---
+++ app/Http/Livewire/StockMaterial.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class StockMaterial extends Component
+{
+    public function render()
+    {
+        return view('livewire.stock-material');
+    }
+}
